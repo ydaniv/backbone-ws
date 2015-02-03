b44d5fe814b3e0f8497de6f43f5580c004566436
ydaniv <maggotfish@gmail.com>
2015-02-03 15:47:38 +0200

    Rewrote the entire implementation into a new WS class that wraps a given resource as first argument

--- backbone-ws.js
+++ backbone-ws.js
@@ -1,13 +1,15 @@
 (function (root, factory) {
-    if (typeof define === 'function' && define.amd) {
+    if ( typeof define === 'function' && define.amd ) {
         // AMD
-        define(['backbone'], function (root, Backbone) {
+        define(['backbone'], function (Backbone) {
             factory(root, Backbone);
         });
-    } else if (typeof exports === 'object') {
+    }
+    else if ( typeof exports === 'object' ) {
         // CommonJS
         factory(root, require('backbone'));
-    } else {
+    }
+    else {
         // Browser globals
         factory(root, root.Backbone);
     }
@@ -15,7 +17,79 @@
 
     var ajaxSync = Backbone.sync;
 
-    Backbone.sync = function (method, model, options) {
+    function WS (resource, url, options) {
+        if ( ! resource ) throw new Error('Resource not provided.');
+        if ( ! url ) throw new Error('URL not provided.');
+        if ( ! (this instanceof WS) ) return new WS(resource, url, options);
+
+        options = options || {};
+
+        this.socket = new root.WebSocket(url, options.protocol);
+        this.typeAttribute = options.typeAttribute || 'type';
+        this.dataAttribute = options.dataAttribute || 'data';
+        this.reopen = 'reopen' in options ? options.reopen : true;
+        this.resource = resource;
+
+        this.socket.onopen = this.onopen.bind(this);
+        this.socket.onmessage = this.onmessage.bind(this);
+        this.socket.onerror = this.onerror.bind(this);
+        this.socket.onclose = this.onclose.bind(this);
+
+        if ( resource instanceof Backbone.Model ) {
+            resource.on('destroy', this.destroy, this);
+        }
+
+        resource[options.sendAttribute || 'send'] = this.send.bind(this);
+
+        if ( options.sync ) {
+            resource.sync = this.sync.bind(this);
+        }
+    }
+
+    WS.prototype.onopen = function () {
+        this.resource.trigger('open');
+    };
+
+    WS.prototype.onmessage = function (event) {
+        var data = JSON.parse(event.data);
+
+        if ( this.typeAttribute && data[this.typeAttribute] ) {
+            data = this.dataAttribute && this.typeAttribute in data ? data[this.dataAttribute] : data;
+
+            this.resource.trigger('message:' + data[this.typeAttribute], data);
+        }
+
+        this.resource.trigger('message', data);
+    };
+
+    WS.prototype.onerror = function () {
+        this.resource.trigger('wserror');
+    };
+
+    WS.prototype.onclose = function (code, reason, wasClean) {
+        this.resource.trigger('close', code, reason, wasClean);
+
+        if ( this.reopen ) {
+            this.socket.open(this.url, this.protocol);
+        }
+    };
+
+    WS.prototype.destroy = function () {
+        this.socket.close();
+        this.socket = null;
+        this.resource = null;
+    };
+
+    WS.prototype.send = function (data) {
+        if ( this.socket ) {
+            this.socket.send(JSON.stringify(data));
+        }
+        else {
+            throw new Error('WebSocket not opened yet!');
+        }
+    };
+
+    WS.prototype.sync = function (method, model, options) {
         if ( options.xhr ) {
             return ajaxSync.call(Backbone, method, model, options);
         }
@@ -24,71 +98,19 @@
 
         data = options.data || options.attrs || model.toJSON(options);
 
-        if (!data.method) {
+        if ( ! data.method ) {
             data.method = method;
         }
 
-        if (typeof options.beforeSend == 'function') {
-            options.beforeSend.apply(this, arguments);
+        if ( typeof options.beforeSend == 'function' ) {
+            options.beforeSend.apply(model, arguments);
         }
 
-        if ( Backbone.ws.socket ) {
-            Backbone.ws.socket.send(JSON.stringify(data));
-        }
-        else {
-            throw new Error('WebSocket not opened yet!');
-        }
+        this.send(data);
 
-        model.trigger('request', model, ws, options);
+        model.trigger('request', model, this.socket, options);
         // returns nothing!
     };
 
-    Backbone.ws = Backbone.Event.extend({
-        typeAttribute: 'type',
-        dataAttribute: 'data',
-        reopen: true,
-        socket: null,
-        url: '',
-        protocol: '',
-        open: function (url, protocol) {
-            var ws = Backbone.ws.socket = new root.WebSocket(url, protocol),
-                type_attr = Backbone.ws.typeAttribute,
-                data_attr = Backbone.ws.dataAttribute;
-
-            if ( url ) {
-                Backbone.ws.url = url;
-            }
-            if ( protocol ) {
-                Backbone.ws.protocol = protocol;
-            }
-
-            ws.onopen = function () {
-                Backbone.ws.trigger('open');
-            };
-
-            ws.onmessage = function (event) {
-                var data = JSON.parse(event.data);
-
-                Backbone.ws.trigger('message', data);
-
-                if (type_attr && data[type_attr]) {
-                    data = data_attr && type_attr in data ? data[data_attr] : data;
-
-                    Backbone.ws.trigger('message:' + data[type_attr], data);
-                }
-            };
-
-            ws.onerror = function () {
-                Backbone.ws.trigger('error');
-            };
-
-            ws.onclose = function (code, reason, wasClean) {
-                Backbone.ws.trigger('close', code, reason, wasClean);
-
-                if (Backbone.ws.reopen) {
-                    Backbone.ws.open(Backbone.ws.url, Backbone.ws.protocol);
-                }
-            };
-        }
-    });
+    return WS;
 }));
